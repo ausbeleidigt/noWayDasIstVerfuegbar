3b290dee4472bdd1d75f8ad0e935a9f617a69bfa
Noah <nk-06@gmx.de>
2022-08-12 09:25:58 +0200

    Tier v0.2

--- Tier.java
+++ Tier.java
@@ -1,3 +1,21 @@
 public class Tier{
+    String art;
+    int groesse;
+    
+    
+    Tier(int groesse, String art){
+        this.groesse = groesse;
+        this.art = art;
+        System.out.println("Das Tier ist " + groesse + " cm groß und ist ein " + art);
+        
+        
+    }
+    
+    
+    void laufen(int strecke){
+        System.out.println("Das Tier ist" + strecke + "cm gelaufen.");
+    }
+     
+    
     
 }
